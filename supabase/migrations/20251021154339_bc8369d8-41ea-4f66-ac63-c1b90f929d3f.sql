74545011ee81da4eed8b604875054d75f501ac6c
Kul Bhaskar Tiwari <kbtiwari228171@gmail.com>
2026-03-16 17:02:41 +0530

    Delete supabase/migrations directory

--- supabase/migrations/20251021154339_bc8369d8-41ea-4f66-ac63-c1b90f929d3f.sql
+++
@@ -1,7 +0,0 @@
--- Update RLS policy to allow viewing all products (including sold ones)
-DROP POLICY IF EXISTS "Anyone can view available products" ON public.products;
-
-CREATE POLICY "Anyone can view all products" 
-ON public.products 
-FOR SELECT 
-USING (true);
